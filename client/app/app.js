cbdae85ec1aa33f337b59bd7769fc83f6c37bd0e
Dolan Miu <dolan_miu@hotmail.com>
2015-04-23 21:32:44 +0100

    added resource end point

--- client/app/app.js
+++ client/app/app.js
@@ -14,3 +14,28 @@ angular.module('doughlandApp').config(function ($stateProvider, $urlRouterProvid
 
     $locationProvider.html5Mode(true);
 });
+
+angular.module('doughlandApp').run(function ($rootScope, LinkedIn, GitHub) {
+    LinkedIn.get({
+        Id: 'getDefault'
+    }, function (profile) {
+        $rootScope.profile = profile;
+        console.log(profile);
+    });
+
+    GitHub.get({}, function (gitHub) {
+        $rootScope.gitHub = gitHub;
+    });
+});
+
+angular.module('doughlandApp').factory('LinkedIn', function ($resource) {
+    'use strict';
+    return $resource('/api/linkedin/:Id', {
+        Id: '@id'
+    });
+});
+
+angular.module('doughlandApp').factory('GitHub', function ($resource) {
+    'use strict';
+    return $resource('https://www.kimonolabs.com/api/azqq1x7m?apikey=0dc941efa2f0a2807d63c75b5010706f');
+});
