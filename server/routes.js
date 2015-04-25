35aa50705d631375dde3b2728ede0d5ee18afc04
Dolan Miu <dolan_miu@hotmail.com>
2015-04-25 19:24:20 +0100

    added github endpoint

--- server/routes.js
+++ server/routes.js
@@ -6,26 +6,24 @@
 
 var errors = require('./components/errors');
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  // Insert routes below
-  app.use('/api/cv', require('./api/cv'));
-  app.use('/api/linkedin', require('./api/linkedin'));
-  app.use('/api/things', require('./api/thing'));
-  
-  // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
-    
-  //Update Page
-  app.route('/profile')
-    .get(function(req, res) {
-      res.sendfile(app.get('appPath') + '/update.html');
+    // Insert routes below
+    app.use('/api/github', require('./api/github'));
+    app.use('/api/cv', require('./api/cv'));
+    app.use('/api/linkedin', require('./api/linkedin'));
+    app.use('/api/things', require('./api/thing'));
+
+    // All undefined asset or api routes should return a 404
+    app.route('/:url(api|auth|components|app|bower_components|assets)/*').get(errors[404]);
+
+    //Update Page
+    app.route('/profile').get(function (req, res) {
+        res.sendfile(app.get('appPath') + '/update.html');
     });
 
-  // All other routes should redirect to the index.html
-  app.route('/*')
-    .get(function(req, res) {
-      res.sendfile(app.get('appPath') + '/index.html');
+    // All other routes should redirect to the index.html
+    app.route('/*').get(function (req, res) {
+        res.sendfile(app.get('appPath') + '/index.html');
     });
 };
