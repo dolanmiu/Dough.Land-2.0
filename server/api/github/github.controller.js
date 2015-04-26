74f357d2e7bbb43f71e18dc8583f59ad396d9b04
Dolan Miu <dolan_miu@hotmail.com>
2015-04-26 01:05:54 +0100

    added a 500 error when cant connect to kimono

--- server/api/github/github.controller.js
+++ server/api/github/github.controller.js
@@ -11,6 +11,8 @@ exports.index = function(req, res) {
     if (response.statusCode == 200) {
       res.setHeader('Content-Type', 'application/json');
       return res.json(200, JSON.parse(body));
+    } else {
+        return res.send(500, "Couldn't get from kimono labs");   
     }
   })
 };
