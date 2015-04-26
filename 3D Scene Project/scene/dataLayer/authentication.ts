01f2366032242fa23f4f3ce2daee469fcce7f0dd
Dolan Miu <dolan_miu@hotmail.com>
2015-04-26 01:13:30 +0100

    added 3D scene project from dough.land 1.0

---
+++ 3D Scene Project/scene/dataLayer/authentication.ts
@@ -0,0 +1,45 @@
+﻿module DoughLand {
+    export class Authentication {
+        private userIP: string;
+        private ableToComment: boolean;
+
+        constructor() {
+            this.userIP = '';
+            this.ableToComment = false;
+        }
+
+        private ajaxCheckIP(ip: string, callback: (res: boolean) => void) {
+            var jsonString = '{"ip":"' + ip + '"}';
+            $.ajax({
+                url: "/ip/check",
+                type: "post",
+                data: jsonString,
+                contentType: 'application/json'
+            })
+                .done(res => {
+                    callback(!res); 
+                });
+        }
+
+        public authenticate(callback: (res: boolean) => void): void {
+            $.getJSON("http://www.telize.com/jsonip?callback=?", json => {
+                this.userIP = json.ip;
+                this.ajaxCheckIP(json.ip, res => {
+                    callback(res);
+                });
+            });
+        }
+
+        public getIP(): string {
+            return this.userIP;
+        }
+
+        public getAbleToComment(): boolean {
+            return this.ableToComment;
+        }
+
+        public setAbleToComment(able: boolean) {
+            this.ableToComment = able;
+        }
+    }
+}
